3aac46e366f276683ff2bb5baaf1d4c144f317f7
Tiago de Oliveira <tiagoliveira.si.2011@gmail.com>
2015-07-15 01:21:16 -0300

    Referencias

---
+++ ProjetoDeBloco.UI/Controllers/AlunoController.cs
@@ -0,0 +1,115 @@
+﻿using ProjetoDeBloco.Aplicacao.Servicos.Interfaces;
+using ProjetoDeBloco.Aplicacao.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoDeBloco.UI.Controllers
+{
+    public class AlunoController : Controller
+    {
+        private IAlunoServico _servico;
+
+        public AlunoController(IAlunoServico servico)
+        {
+            _servico = servico;
+        }
+
+        // GET: Aluno
+        public ActionResult Index()
+        {
+            var alunos = _servico.ListarTodos();
+            return View(alunos);
+        }
+
+        // GET: Aluno/Details/5
+        public ActionResult Visualizar(Guid id)
+        {
+            var aluno = _servico.BuscarPorId(id);
+            return View(aluno);
+        }
+
+        // GET: Aluno/Create
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        // POST: Aluno/Create
+        [HttpPost]
+        public ActionResult Cadastrar(AlunoVM model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            try
+            {
+                _servico.Cadastrar(model);
+
+                ModelState.Clear();
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("listaDeErros", ex.Message);
+                return View(model);
+            }
+        }
+
+        // GET: Aluno/Edit/5
+        public ActionResult Editar(Guid id)
+        {
+            var aluno = _servico.BuscarPorId(id);
+            return View(aluno);
+        }
+
+        // POST: Aluno/Edit/5
+        [HttpPost]
+        public ActionResult Editar(AlunoVM model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            try
+            {
+                _servico.Cadastrar(model);
+
+                ModelState.Clear();
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("listaDeErros", ex.Message);
+                return View(model);
+            }
+        }
+
+        // GET: Aluno/Delete/5
+        public ActionResult Remover(Guid id)
+        {
+            var aluno = _servico.BuscarPorId(id);
+            return View(aluno);
+        }
+
+        // POST: Aluno/Delete/5
+        [HttpPost]
+        public ActionResult Remover(AlunoVM model)
+        {
+            try
+            {
+                _servico.Remover(model);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("listaDeErros", ex.Message);
+                return View(model);
+            }
+        }
+    }
+}
