4e674826898ebeee519df231dde4a20a79eaf05d
Tiago de Oliveira <tiagoliveira.si.2011@gmail.com>
2015-08-06 16:03:46 -0300

    Merge

---
+++ ProjetoDeBloco.UI/Controllers/AvaliacaoController.cs
@@ -0,0 +1,27 @@
+﻿using ProjetoDeBloco.Aplicacao.Servicos.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoDeBloco.UI.Controllers
+{
+    public class AvaliacaoController : Controller
+    {
+
+        public IAvaliacaoServico _servicoAvaliacao;
+        public ITurmaServico _servicoTurma;
+
+        public AvaliacaoController()
+        {
+
+        }
+        //
+        // GET: /Avaliacao/
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
