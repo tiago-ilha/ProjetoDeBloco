73c2ec7334361c9fb633cce139142ec4f1a86a5a
Tiago de Oliveira <tiagoliveira.si.2011@gmail.com>
2015-12-10 01:23:55 -0200

    Classe utilitária para envio de emails.

---
+++ ProjetoDeBloco.Utilitarios/ServicoEmail/EmailUtil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoDeBloco.Utilitarios.ServicoEmail
+{
+    public static class EmailUtil
+    {
+        public static void EnviarEmail(string para, string assunto, string corpo)
+        {
+
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("projetodeblocoinfnet@gmail.com");
+            mail.To.Add(para);
+            mail.Subject = assunto;
+            mail.Body = corpo;
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            smtp.Send(mail);
+        }
+    }
+}
